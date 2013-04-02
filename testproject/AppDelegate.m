01821b4c1093e85cae724b57b420aa26bdaa28f2
Michelle Ramirez <michelle@thecellcity.com>
2013-04-02 16:48:24 +0800

    Add Files to Project from Submodule

--- testproject/AppDelegate.m
+++ testproject/AppDelegate.m
@@ -11,7 +11,7 @@
 #import "FirstViewController.h"
 
 #import "SecondViewController.h"
-
+#import "FMInfoPanelViewController.h"
 @implementation AppDelegate
 
 - (void)dealloc
@@ -26,7 +26,7 @@
     self.window = [[[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]] autorelease];
     // Override point for customization after application launch.
     UIViewController *viewController1 = [[[FirstViewController alloc] initWithNibName:@"FirstViewController" bundle:nil] autorelease];
-    UIViewController *viewController2 = [[[SecondViewController alloc] initWithNibName:@"SecondViewController" bundle:nil] autorelease];
+    UIViewController *viewController2 = [[[FMInfoPanelViewController alloc] initWithNibName:@"FMInfoPanelView" bundle:nil] autorelease];
     self.tabBarController = [[[UITabBarController alloc] init] autorelease];
     self.tabBarController.viewControllers = @[viewController1, viewController2];
     self.window.rootViewController = self.tabBarController;
